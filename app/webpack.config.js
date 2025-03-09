34ca6daf0204327e87f1363b5043bfac5f7f0aa0
Tara niaki <taraniaki@gmail.com>
2025-03-09 18:45:55 -0400

    last commit

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -48,7 +48,7 @@ module.exports = {
         generator: {
           filename: 'assets/fonts/[name][hash][ext][query]', // Colocando as fontes dentro de assets/fonts/
         },
-      },      
+      },
     ],
   },
   plugins: [
@@ -60,16 +60,16 @@ module.exports = {
       chunks: ["global", "index"],
       filename: "index.html",
     }),
-    // new HtmlWebpackPlugin({
-    //   template: "./list-products.html",
-    //   chunks: ["listProducts", "global"],
-    //   filename: "list-products.html",
-    // }),
-    // new HtmlWebpackPlugin({
-    //   template: "./add-products.html",
-    //   chunks: ["addProducts", "global"],
-    //   filename: "add-products.html",
-    // }),
+    new HtmlWebpackPlugin({
+      template: "./list-products.html",
+      chunks: ["listProducts", "global"],
+      filename: "list-products.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./add-products.html",
+      chunks: ["addProducts", "global"],
+      filename: "add-products.html",
+    }),
   ],
   optimization: {
     minimize: true,
